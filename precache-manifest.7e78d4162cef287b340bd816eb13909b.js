e4ed9a21250491a42b9b8e7f8e994617a566704d
MilkyLatte <jpqo9508@gmail.com>
2019-04-07 22:43:55 +0100

    Updates

---
+++ precache-manifest.7e78d4162cef287b340bd816eb13909b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfc092697ec5e2f98abc",
+    "url": "/movieapp/static/css/main.b9b9b38f.chunk.css"
+  },
+  {
+    "revision": "dfc092697ec5e2f98abc",
+    "url": "/movieapp/static/js/main.31cc2074.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  },
+  {
+    "revision": "5060ec8cb8bb10249054",
+    "url": "/movieapp/static/js/2.d78e0c44.chunk.js"
+  },
+  {
+    "revision": "0722a26174a61ecbc546c992c1812278",
+    "url": "/movieapp/index.html"
+  }
+];
